20a09c660ef43e8ff3d01026d87ef04e68b6c19e
Deepak singh <deepakbaghel.official20@gmail.com>
2026-01-23 18:34:27 +0530

    second commit

--- src/Auth/login.jsx
+++ src/Auth/login.jsx
@@ -1,19 +1,6 @@
-// function Login() {
-//   return (
-//     <div style={{ padding: "40px" }}>
-//       <h1>Invoice Login</h1>
-//       <p>Frontend is now wired correctly.</p>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
 
 import { useState } from "react";
-import axios from "../Apis/axiosConfig";
-
-
+import axios from "../apis/axiosConfig";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -22,53 +9,38 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/auth/login", {
-        username,
-        password,
-      });
-
-      localStorage.setItem("token", res.data.token);
-      alert("Login successful");
-    // } catch (err) {
-    //   alert("Invalid credentials");
-    // }
-
-    }   catch (err) {
-  console.log("FULL ERROR:", err);
-  console.log("BACKEND RESPONSE:", err.response);
-
-  alert(
-    err.response?.data?.message || 
-    "Login failed (no message from backend)"
-  );
-}
-}
-  
+      await axios.post("/auth/login", { username, password });
+      alert("Login success");
+    } catch {
+      alert("Invalid credentials");
+    }
+  };
 
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Invoice Login</h1>
+    <div className="login-page">
+      <div className="login-box">
+        <h1>Invoice Login</h1>
 
-      <form onSubmit={handleLogin}>
-        <input
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br /><br />
+        <form onSubmit={handleLogin}>
+          <input
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br /><br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <button type="submit">Login</button>
-      </form>
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
-};
+}
 
 export default Login;
+
